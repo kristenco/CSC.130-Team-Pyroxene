6d6e53c7a768bbb1e6de85f501e269080e2376fb
Kristen Dine <kcdine@my.waketech.edu>
2022-12-01 20:33:54 -0500

    fixed up menu, started on waitlist based on queue homework but needs lots of fixing

--- course_functions.py
+++ course_functions.py
@@ -7,22 +7,142 @@
         - Paying for the courses
 """
 
+"""
+    class ListQueue(object):
+        def __init__(self, sourceCollection=None):
+            self.items = []
+            self.size = 0
+            if sourceCollection:
+                self.add(sourceCollection)
 
+        def __len__(self):
+            return self.size
+
+        def __iter__(self):
+            for i in self.items:
+                yield i
+
+        def __str__(self):
+            return "{" + ",".join(map(str, self)) + "}"
+
+        def __add__(self, other):
+            ret_val = ListQueue(self)
+            for item in other:
+                ret_val.add(item)
+            return ret_val
+
+        def add(self, item):
+            self.items.append(item)
+            self.size += 1
+
+        def pop(self):
+            if self.isEmpty():
+                raise IndexError("Queue is empty")
+            else:
+                val = self.items[0]
+                del self.items[0]
+                return val
+"""
+
+class ListQueue(object):
+    """A list-based stack implementation."""
+
+    # Constructor
+    def __init__(self, sourceCollection=None):
+        """Sets the initial state of self, which includes the
+        contents of sourceCollection, if it's present."""
+        self.items = []
+        self.size = 0
+        if sourceCollection:
+            for item in sourceCollection:
+                self.add(item)
+
+    # Accessor methods
+    def isEmpty(self):
+        """Returns True if the queue is empty, or False otherwise."""
+        return len(self.items) == 0
+
+    def __len__(self):
+        """Returns the number of items in the queue."""
+        return self.size
+
+    def __str__(self):
+        """Returns the string representation of the queue."""
+        return "{" + ",".join(map(str, self)) + "}"
+
+    def __iter__(self):
+        """Supports iteration over a view of the queue."""
+        for i in self.items:
+            yield i
+
+    def __add__(self, other):
+        """Returns a new queue containing the contents
+        of self and other."""
+        ret_val = ListQueue(self)
+        for item in other:
+            ret_val.add(item)
+        return ret_val
+
+    def __eq__(self, other):
+        """Returns True if self equals other,
+        or False otherwise."""
+        for i in self.items:
+            if len(self.items) != len(other):
+                return False
+        return True
+
+    def peek(self):
+        """Returns the item at the front of the queue.
+        Precondition: the queue is not empty.
+        Raises IndexError if queue is empty."""
+        if self.isEmpty():
+            raise IndexError("Index is empty")
+        else:
+            for i in self.items:
+                if i == self.items[0]:
+                    return i
+
+    # Mutator methods
+    def clear(self):
+        """Makes self become empty."""
+        self.items.clear()
+
+    def add(self, item):
+        """Inserts item at rear of the queue."""
+        self.items.append(item)
+        self.size += 1
+
+    def pop(self):
+        """Removes and returns the item at the front of the queue.
+        Precondition: the queue is not empty.
+        Raises IndexError if queue is not empty.
+        Postcondition: the front item is removed from the queue."""
+        if self.isEmpty():
+            raise IndexError("Queue is empty")
+        else:
+            val = self.items[0]
+            del self.items[0]
+            return val
 class Courses:
     def __init__(self):
         self.courses = []
 
+
+
     def add_course(self):
+        seats = 0
         course_input = (input('Enter the course you\'d like to add: ')).upper()
         if course_input in self.courses:
-            print('Class already exists.')
+            print('You\'ve already registered for this class.')
+        elif seats <= 0:
+            waitlist = ListQueue(self)
+            print(str(waitlist))
+            print(f"\n\nThis class registration is currently full. You've been added to the waitlist.\n"
+            f"The number of students in the current waitlist: {len(waitlist)}")
         else:
             self.courses.append(course_input)
+            seats -= 1
             print(f'Course {course_input} was added.')
-
-    def wait_list_student(self):
-        # TODO
-        pass
 
     def remove_course(self):
         course_input = input('Enter the course you\'d like to remove or enter "All" to remove all: ').upper()
--- menu.py
+++ menu.py
@@ -3,20 +3,31 @@ from course_functions import Courses
 if __name__ == "__main__":
     # Continue offering selection menu until user exits.
     courses = Courses()
-    menu = input('Course Menu\n1 - Add a course.\n2 - Add student to a wait list.\n3 - Remove a course.\n'
-                 '4 - List the courses.\n5 - Pay for the courses.\n0 - To exit.\nEnter your choice: ')
-    while menu != '0':
+    login = True
+
+    while True:
+        while login:
+            print(f"Student Registration Login:")
+            name = input("Name: ")
+            id = input("Student ID: ")
+            login = False
+        print()
+        menu = input('Course Menu\n1 - Add a course.\n2 - Remove a course.\n'
+                     '3 - List the courses.\n4 - Pay for the courses.\n5 - Log in as different student\n0 - To exit.\nEnter your choice: ')
         if menu == '1':
             courses.add_course()
         elif menu == '2':
-            courses.wait_list_student()
-        elif menu == '3':
             courses.remove_course()
-        elif menu == '4':
+        elif menu == '3':
             courses.list_courses()
-        elif menu == '5':
+        elif menu == '4':
             courses.pay_for_courses()
+        elif menu == '5':
+            login = True
+            print()
+        elif menu == '0':
+            print("\nExiting...")
+            break
         else:
             print('Invalid Choice.')
-        menu = input('Course Menu\n1 - Add a course.\n2 - Add student to a wait list.\n3 - Remove a course.\n'
-                     '4 - List the courses.\n5 - Pay for the courses.\n0 - To exit.\nEnter your choice: ')
+        
